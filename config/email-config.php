c0aa68084ac534adcb7043e9194881436c6a4ef5
DerickMuluka <mulukaderick@gmail.com>
2025-08-30 10:07:03 +0300

    chore: add email configuration settings for SMTP service

---
+++ config/email-config.php
@@ -0,0 +1,14 @@
+<?php
+// Email configuration settings
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 587);
+define('SMTP_USERNAME', 'greatdev39@gmail.com');
+define('SMTP_PASSWORD', 'ybwe jqnw fitj yuyv');
+define('SMTP_FROM', 'greatdev39@gmail.com');
+define('SMTP_FROM_NAME', 'The Hub Computers Website');
+define('SMTP_REPLY_TO', 'greatdev39@gmail.com');
+define('SMTP_REPLY_TO_NAME', 'The Hub Computers Support');
+
+// For debugging (set to 0 in production)
+define('SMTP_DEBUG', 0);
+?>
